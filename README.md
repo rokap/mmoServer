76230d6b940a667de188e113077cf8267c88824f
David Kaplan <dave@rokap.com>
2016-05-13 10:54:55 -0400

    Updated Package

--- README.md
+++ README.md
@@ -4,6 +4,11 @@
 - nodejs - https://nodejs.org/en/
 
 ## Install
+1. Download and install nodejs
+2. navigate to your project folder
+3. hold SHIFT + Right click -> open cammand prompt here ( windows )
+4. type 'npm install socket.io mysql'
+5. drink coffee
 
 ## Starting Server
 
--- index.js
+++ index.js
@@ -1,92 +1,40 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+// Load/Setup Libraries
 var mysql = require('mysql');
+var io = require('socket.io')({
+    transports: ['websocket']
+});
 
-// Define our db creds
+// Define our db credentials
 var db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'hobbes03',
-    database: 'chat'
-})
+    password: '',
+    database: 'database'
+});
 
 // Log any errors connected to the db
 db.connect(function (err) {
     if (err) console.log(err)
 });
 
-var loggedIn = false;
+// Attach a Listener
+io.attach(4567);
+
+// Start Events
 io.on('connection', function (socket) {
-
-    var client = {
-        net_id: socket.id,
-        account_id: 0,
-        username: 'Anonymous',
-        email: ''
-    };
-
-    // Connected
-    io.emit('client connected', client);
-    console.log(socket.id + ': connected');
-
-    // Login
-    socket.on('login', function (data) {
-
-        db.query("SELECT * FROM accounts where email = '" + data.email + "' && pass = '" + data.pass + "' && active = 1")
-            .on('result', function (data) {
-                loggedIn = true;
-                client.account_id = data.id;
-                client.email = data.email;
-            })
-            .on('end', function () {
-                // Only emit notes after query has been completed
-                if (loggedIn) {
-                    console.log(socket.id + ": " + data.email + " - Login Success (" + client.account_id + ")");
-
-                    socket.emit('client:login', {loggedIn: loggedIn, error: ''})
-                } else {
-                    loggedIn = false;
-                    console.log(socket.id + ": " + data.email + " - Username/Password Incorrect");
-                    socket.emit('client:login', {loggedIn: loggedIn, error: 'Username/Password Incorrect'})
-                }
-            });
-    });
-
-    // Logout
-    socket.on('logout', function (data) {
-        loggedIn = false;
-        console.log(socket.id + ": " + client.email + " - Logged Out (" + client.account_id + ")");
-        socket.emit('client:logout', {loggedIn: loggedIn})
-    });
-
-    // Disconnect
+    var netID = socket.id;
+    Debug(socket,"Connected");
     socket.on('disconnect', function () {
-
-        // Client Connected
-        console.log(socket.id + ': disconnected');
-        io.emit('client disconnected', client)
-    });
-
-    // Request Characters
-    socket.on('server:requestCharacters', function () {
-
-        // requestCharacters
-        console.log(socket.id + ': server:requestCharacters - ' + client.account_id);
-
-        var characters = [];
-
-        db.query("SELECT * FROM characters where account_id = " + client.account_id + "")
-            .on('result', function (data) {
-                characters.push(data)
-            })
-            .on('end', function () {
-                socket.emit('client:characters', { characters:characters })
-            });
+        Debug(socket,"Disconnected");
     });
 });
 
-
-http.listen(3000, function () {
-    console.log('listening on *:3000');
-});
+function Debug(s, m) {
+    var d = getFormattedDate();
+    console.log(d + " | " + s + ": " + m);
+}
+function getFormattedDate() {
+    var date = new Date();
+    var str = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    return str;
+}
