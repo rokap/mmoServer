aebc1946b1cccad06d90df288bd0b8e75dbce090
David Kaplan <dave@rokap.com>
2016-05-15 00:43:33 -0400

    Updated Package

--- events/account.js
+++ events/account.js
@@ -10,7 +10,7 @@ module.exports = function (io, socket, db) {
     };
 
     socket.on("server:register", function (data) {
-        utilities.debug(socket, "server:register");
+        utilities.debug(socket, "server:register (" + JSON.stringify(data) + ")");
         var accountExists = false;
         // Check if account exists already
         db.query("SELECT * FROM accounts where username = '" + data.username + "'")
@@ -37,7 +37,7 @@ module.exports = function (io, socket, db) {
     });
 
     socket.on("server:login", function (data) {
-        utilities.debug(socket, "server:login");
+        utilities.debug(socket, "server:login (" + JSON.stringify(data) + ")");
 
         var loggedIn = false;
 
@@ -52,23 +52,96 @@ module.exports = function (io, socket, db) {
                 // Only emit notes after query has been completed
                 if (loggedIn) {
                     utilities.debug(socket, account.username + " - Login Success (" + account.account + ")");
-                    socket.emit('client:login', {loggedIn: loggedIn, error: ''})
+                    socket.emit('client:login', {loggedIn: loggedIn, account: account, error: ''})
                 } else {
                     loggedIn = false;
                     utilities.debug(socket, account.username + " - Username/Password Incorrect");
-                    socket.emit('client:login', {loggedIn: loggedIn, error: 'Username/Password Incorrect'})
+                    socket.emit('client:login', {
+                        loggedIn: loggedIn,
+                        account: account,
+                        error: 'Username/Password Incorrect'
+                    })
                 }
             });
 
 
     });
 
-    socket.on("server:logout", function (data) {
+    socket.on("server:logout", function () {
         utilities.debug(socket, "server:logout");
-        // Handle data
-        // Send response to client
-        socket.emit("client:logout", data);
+        account = {
+            id: 0,
+            account: 0,
+            username: 'Anonymous',
+            email: ''
+        };
+        socket.emit("client:logout", account);
 
+    });
+
+    socket.on("server:requestCharacters", function () {
+
+        var characters = [];
+
+        utilities.debug(socket, "server:requestCharacters");
+        db.query("SELECT `id`,  `name`,  `class`,  `level` FROM characters WHERE account = ? ", account.account)
+            .on('result', function (data) {
+                characters.push(data);
+            })
+            .on('end', function () {
+                socket.emit("client:requestCharacters", {characters: characters});
+            });
+
+    });
+
+    socket.on("server:selectCharacter", function (data) {
+
+        var character = {};
+
+        utilities.debug(socket, "server:selectCharacter (" + JSON.stringify(data) + ")");
+        db.query("SELECT `id`,  `name`,  `class`,  `level`,  `posX`,  `posY`,  `posZ`,  `rot` FROM characters WHERE id = ? && account = ? ", [data.id, account.account])
+            .on('result', function (data) {
+                character = data;
+            })
+            .on('end', function () {
+                socket.emit("client:selectCharacter", character);
+            });
+
+    });
+
+    socket.on("server:createCharacter", function (data) {
+
+        utilities.debug(socket, "server:createCharacter (" + JSON.stringify(data) + ")");
+
+        var createCharacter = false;
+        // Check if account exists already
+        db.query("SELECT * FROM characters where name = '" + data.name + "'")
+            .on('result', function (data) {
+                createCharacter = true;
+            })
+            .on('end', function () {
+                if (createCharacter) {
+                    // Already Exists
+                    utilities.debug(socket, "Character " + data.name + " Already Exists");
+                    socket.emit('client:createCharacter', {characterExists: createCharacter})
+                } else {
+                    // Available, Create account
+                    utilities.debug(socket, "Character " + data.name + " is available, Creating");
+                    db.query(
+                        "INSERT INTO characters (name,account,class,level) VALUES (?,?,?,?)",
+                        [data.name, account.account, data.class, 1]
+                    );
+                    socket.emit('client:createCharacter', {characterExists: createCharacter})
+                }
+            });
+
+
+    });
+
+    socket.on("server:deleteCharacter", function (data) {
+        utilities.debug(socket, "server:deleteCharacter (" + JSON.stringify(data) + ")");
+        db.query("DELETE FROM characters where id = '" + data.id + "'");
+        socket.emit("client:deleteCharacter", {deleted: true})
     });
 
 };
