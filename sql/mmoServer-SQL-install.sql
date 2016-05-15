90b912ab062b862a7197a188cc2a183528994e2f
David Kaplan <dave@rokap.com>
2016-05-15 11:31:50 -0400

    Updated Package

--- sql/mmoServer-SQL-install.sql
+++ sql/mmoServer-SQL-install.sql
@@ -10,6 +10,20 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
+-- Dumping structure for table tlserver.access_log
+CREATE TABLE IF NOT EXISTS `access_log` (
+  `id`        INT(11)   NOT NULL AUTO_INCREMENT,
+  `date`      TIMESTAMP NULL     DEFAULT CURRENT_TIMESTAMP,
+  `ipaddress` VARCHAR(50)        DEFAULT NULL,
+  `session`   VARCHAR(50)        DEFAULT NULL,
+  PRIMARY KEY (`id`)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = latin1;
+
+-- Data exporting was unselected.
+
+
 -- Dumping structure for table tlserver.accounts
 CREATE TABLE IF NOT EXISTS `accounts` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -27,15 +41,16 @@ CREATE TABLE IF NOT EXISTS `accounts` (
 
 -- Dumping structure for table tlserver.characters
 CREATE TABLE IF NOT EXISTS `characters` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `create` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `id`      int(11) NOT NULL AUTO_INCREMENT,
+  `create`  timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `account` int(11) NOT NULL DEFAULT '0',
-  `name` varchar(50) DEFAULT NULL,
-  `level` int(11) DEFAULT NULL,
-  `posX` float(12,6) DEFAULT NULL,
-  `posY` float(12,6) DEFAULT NULL,
-  `posZ` float(12,6) DEFAULT NULL,
-  `rot` float(12,6) DEFAULT NULL,
+  `name`    varchar(50) DEFAULT NULL,
+  `class`   VARCHAR(50) DEFAULT NULL,
+  `level`   int(11) DEFAULT NULL,
+  `posX`    FLOAT(12, 6) DEFAULT '0.000000',
+  `posY`    FLOAT(12, 6) DEFAULT '0.000000',
+  `posZ`    FLOAT(12, 6) DEFAULT '0.000000',
+  `rot`     FLOAT(12, 6) DEFAULT '0.000000',
   PRIMARY KEY (`id`),
   KEY `FK_characters_accounts` (`account`),
   CONSTRAINT `FK_characters_accounts` FOREIGN KEY (`account`) REFERENCES `accounts` (`id`)
