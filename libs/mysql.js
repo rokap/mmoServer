380603b2432394dcd6113f8b12ea934602c8cfdf
David Kaplan <dave@rokap.com>
2016-05-15 10:39:39 -0400

    Updated Package

--- libs/mysql.js
+++ libs/mysql.js
@@ -2,9 +2,14 @@ var utilities = require('../libs/utilities');
 var mysql = require('mysql');
 
 module.exports = function (config) {
-    var db = mysql.createConnection({host: config.db.host, user: config.db.user, password: config.db.password, database: config.db.database});
+    var db = mysql.createConnection({
+        host: config.db.host,
+        user: config.db.user,
+        password: config.db.password,
+        database: config.db.database
+    });
     db.connect(function (err) {
-        utilities.debug("Core", "Using Mysql");
+        utilities.debug("Core", "Using Mysql (" + config.db.database + ")");
         if (err) console.log(err);
     });
     return db;
