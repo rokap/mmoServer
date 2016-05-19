006a2fb40f7677395272a0e8cc79f283491eabfd
David Kaplan <dave@rokap.com>
2016-05-19 11:16:39 -0400

    Added Auto Mysql Updater

--- libs/mysql.js
+++ libs/mysql.js
@@ -12,6 +12,40 @@ module.exports = function (config) {
         utilities.debug("Core", "Using Mysql (" + config.db.database + ")");
         if (err) console.log(err);
     });
+
+    // CREATE if missing sqlupdates
+    db.query('CREATE TABLE IF NOT EXISTS `sqlupdates` (' +
+        '`id` INT(11) NOT NULL AUTO_INCREMENT,' +
+        '`file` VARCHAR(50) NULL DEFAULT NULL,' +
+        'PRIMARY KEY (`id`)' +
+        ')' +
+        'ENGINE=InnoDB' +
+        ';');
+
+    var fs = require('fs');
+    fs.readdir('./sql/', function (err, files) {
+        if (err) throw err;
+
+        files.forEach(function (file) {
+            var sqlUpdate = file.substring(0, file.length - 4);
+            var exists = false;
+            db.query("SELECT * FROM sqlupdates WHERE file=?", sqlUpdate)
+                .on('result', function () {
+                    // it exists!
+                    exists = true;
+                })
+                .on('end', function () {
+                    if (!exists) {
+                        console.log("Added " + sqlUpdate + " to db");
+
+                        var sql = fs.readFileSync("./sql/" + file).toString();
+                        db.query(sql);
+                        db.query("INSERT INTO sqlupdates (file) VALUES (?)", sqlUpdate);
+                    }
+                })
+        });
+    });
+
     return db;
 };
 
