a7ee730f2d39308f6888040aaf5b2a91fbd6761e
David Kaplan <dave@rokap.com>
2016-05-19 11:18:11 -0400

    FIXED SQL Files

--- sql/mmoServer-SQL-install.sql
+++ sql/mmoServer-SQL-install.sql
@@ -5,11 +5,6 @@
 -- HeidiSQL Version:             9.3.0.4984
 -- --------------------------------------------------------
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
 -- Dumping structure for table tlserver.access_log
 CREATE TABLE IF NOT EXISTS `access_log` (
   `id`        INT(11)   NOT NULL AUTO_INCREMENT,
