592bd5e4e3de2389450b17e03238a7afa7872c4c
David Kaplan <dave@rokap.com>
2016-06-01 09:29:21 -0400

    Fixed Spell Array

--- events/account.js
+++ events/account.js
@@ -229,7 +229,7 @@ module.exports = function (server, io, db) {
 
         function getSpells(callback)
         {
-            db.query("SELECT id,name,type,castTime,resource,sParticles,dParticles FROM characterspells as cs INNER JOIN spell as s ON cs.spell_id = s.id WHERE cs.character_id = ?", tmpCharacter.id).on("result", function (spell) {
+            db.query("SELECT id,name,icon,type,castTime,resource,sParticles,dParticles FROM characterspells as cs INNER JOIN spell as s ON cs.spell_id = s.id WHERE cs.character_id = ?", tmpCharacter.id).on("result", function (spell) {
                 var data = {
                     id: spell.id,
                     name: spell.name,
